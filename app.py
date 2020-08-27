29c61eb07a4814a7239aef401cac69f3ca155046
Arinzea <ogbuchiarinze01@gmail.com>
2020-08-27 14:41:27 +0100

    Almost done , it remains fixing the alert problem

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask,render_template,request
+import datetime
+app = Flask(__name__)
+
+@app.route('/',methods=['POST','GET'])
+def hello():
+    now = datetime.datetime.now()
+    if request.method == 'POST':
+        
+        try:
+            weight = request.form.get('weight')
+            height = request.form.get('height')
+            newweight = float(weight)
+            newheight = float(height)
+            if (newheight and newheight) != str or (newheight or newheight) != str :
+                result = round((newweight /((newheight/100) ** 2)),2)
+                context = {
+                    "now":now,
+                    "result":result,
+                    
+                }
+            
+                return render_template('index.html',**context)
+        
+
+
+        except ValueError as error:
+            result = False
+            error = "You inputed the wrong value,kindly input an integer."
+            return render_template('index.html',error=error)
+    
+
+    elif request.method == 'GET':
+        return render_template('index.html')
+        
+        
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
